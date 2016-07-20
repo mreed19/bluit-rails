cf820e4ccf1bf7eb0c5a026352e0092844f6134f
mreed19 <michael.reed315@gmail.com>
2016-07-20 15:20:34 -0400

    Use a helper method to expose categories to views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :categories
+
+  private
+
+  def categories
+    @categories ||= Category.all
+  end
 end
